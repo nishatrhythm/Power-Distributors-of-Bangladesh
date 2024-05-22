c904287d11e8d6eb9d09c66de56d897b7a2ffc99
NISHAT MAHMUD <nishatrhythm@gmail.com>
2024-05-22 12:47:20 +0600

    Updates

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,11 @@
 from flask import jsonify
 from app import app
+import os
+import json
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
-    with open('app/data/power_distributors.json', 'r') as file:
-        data = file.read()
+    json_path = os.path.join(app.root_path, 'data', 'power_distributors.json')
+    with open(json_path, 'r') as file:
+        data = json.load(file)
     return jsonify(data)
